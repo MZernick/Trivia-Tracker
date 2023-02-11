31f3fd4b3dd689cfebd182abfb43c255501542f6
Moonbeam <mfzernick@gmail.com>
2023-02-11 11:26:17 -0600

    some routes running

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,7 @@
 const sequelize = require('../config/connection');
 const { User, Project } = require('../models');
 
-const triviaQ = require('./trivia-questions.json');
+const userData = require('./userData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -10,13 +10,6 @@ const seedDatabase = async () => {
     individualHooks: true,
     returning: true,
   });
-
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
 
   process.exit(0);
 };
