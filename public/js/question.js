2d92ddfe201d34759453d3041478cec5c4b9c417
Erin <erinpeifer@gmail.com>
2023-02-11 01:06:59 -0600

    questions.js is functional to populate questions and check answers; ADDED 2 CLASSES TO style.css; NEED TO MOVE erin.html to trivia.handlebars

--- public/js/question.js
+++ public/js/question.js
@@ -1,94 +1,78 @@
-// https://the-trivia-api.com/docs/#search
-var question = document.querySelector("#question");
+var questionEl = document.querySelector("#question");
 
 var questions = [];      
 var currentQuestion = 0;
-
-// function startGame() {
-//           getQuestions()
-//           renderQuestions()
-// }
+var score = 0
 
 async function getQuestions() { 
     const fetchedQs = await fetch('https://the-trivia-api.com/api/questions?limit=20', {
-        // method: 'GET',
         headers: {
             'Content-Type': 'application/json'
         },
-    }).then ((response) => 
+    })
+    .then ((response) => 
     response.json())
-    .then(
-        (fetchedQs) => {
-    questions = fetchedQs
-    // var answers = [questions[0][1].incorrectAnswers + "," + questions[0][1].correctAnswer]
-    console.log(questions)
-    // console.log(fetchedQs[1].question)
-    renderQuestions(fetchedQs)
-});
-
-    }
-getQuestions()
-
-function renderQuestions(fetchedQs) {
-    var questionIndex = fetchedQs[currentQuestion];
-    console.log(questionIndex.question);
-    question.textcontent = questionIndex.question;
-
-            // console.log(fetchedQs.length)
-            // var qArr = questions[0];
-        // var answers = [qArr[i].incorrectAnswers + "," + qArr[i].correctAnswer];
-        // var questionIndex = qArr[i];
-
-        // btns.innerHTML = "";
-        // questionIndex.answers.forEach(answer => {
-        //     var button = docuemnt.createElement("button");
-        //     button.setAttribute("class", "btn")
-        //     button.setAttribute("value", answer)
-        //     button.textContent = answer;
-        // })
-
+    .then((fetchedQs) => {
+            questions = fetchedQs
+            console.log(questions)
+            renderQuestions(questions)
+    })
     }
 
-
-
-// var startBtn = document.querySelector(".start-button");
-// var timerEl = document.querySelector(".timer-count");
-// var startContainer = document.querySelector(".start-container");
-// var questionContainer = document.querySelector(".question-container");
-// var question = document.querySelector("#question");
-// var btns = document.querySelector("#btns");
-// var scoreContainer = document.querySelector(".score-container");
-// var finalScore = document.querySelector("#final-score")
-// var inputEl = document.querySelector("#name-input")
-// var submitBtn = document.querySelector("#submit-btn")
-// var timer;
-// var timerCount = 60
-// var currentQuestion = 0;
-//   startContainer.style.display = 'none';
-//   questionContainer.style.display = 'block';
-
-//   question.textContent = questionIndex.question;
-//   btns.innerHTML = "";
-//   questionIndex.answers.forEach(answer => {
-//     var button = document.createElement("button");
-//     button.setAttribute("class", "btn")
-//     button.setAttribute("value", answer)
-//     button.textContent = answer;
-//     button.onclick = checkAnswer;
-//     btns.appendChild(button);
-//       })
+function shuffleArray(arr) {
+        for (var i = arr.length - 1; i > 0; i--) {
     
+            var j = Math.floor(Math.random() * (i + 1));
+    
+            var temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+        return arr;
+    }
+var correct;
+function renderQuestions() {
 
-// var checkAnswer = function() {
-//       if (this.value !== quizQuestions[currentQuestion].correct) {
-//         timerCount -=10;
-//         timerEl.textContent = "Timer: " + timerCount;
-//       } else {
-//       } 
-// currentQuestion++;
-//       if (currentQuestion === quizQuestions.length) {
-//         endQuiz()
-//       } else {
-//         renderQuestions()
-//       }
-//     }
+    var questionIndex = questions[currentQuestion];
+    console.log(questionIndex.question);
+    correct = questionIndex.correctAnswer
+
+    questionEl.textContent = questionIndex.question
+    var answers = questionIndex.incorrectAnswers;
+    answers.push(questionIndex.correctAnswer);
+    console.log(answers)
+    var randomAnswers = shuffleArray(answers)
+    console.log(randomAnswers)
+    btns.innerHTML = "";
+    randomAnswers.forEach(answer => {
+        var button = document.createElement("button");
+        button.setAttribute("class", "text-center rounded-pill answer-btn")
+        button.setAttribute("style", "background-color: #389393;")
+        button.setAttribute("value", answer)
+        button.textContent = answer;
+        button.onclick = checkAnswer;
+        btns.appendChild(button);
+        },
+        )
+}
+
+var checkAnswer = function() {
+    console.log(questions[currentQuestion].correctAnswer)
+    if (this.value !== questions[currentQuestion].correctAnswer) {
+    console.log("incorrect"),
+    currentQuestion++;
+    //   timerCount -=10;
+    //   timerEl.textContent = "Timer: " + timerCount;
+    } else {
+        console.log("correct"),
+        currentQuestion++;
+    }
+
+    if (currentQuestion === questions.length) {
+        console.log("game over")
+        //   endQuiz()
+    } else {
+      renderQuestions()
+    }
+  }
+getQuestions()
