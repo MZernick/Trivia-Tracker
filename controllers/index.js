3905cd6095ff9dead5035c5616077704341dad80
Erin <erinpeifer@gmail.com>
2023-02-11 21:24:59 -0600

    reverted main.handlebars added nav bar; trivia renders; need to connect to js

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes= require('./htmlroutes.js/index')
 const apiRoutes = require('./api');
+const gameRoutes = require('./gameRoutes')
 
 router.use('/api', apiRoutes);
 router.use('/', userRoutes);
+router.use('/trivia', gameRoutes)
 
 module.exports = router;
