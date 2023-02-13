5b9a137698e9a5331ca2b6ae72e60a145f43a1c8
Moonbeam <mfzernick@gmail.com>
2023-02-13 12:04:25 -0600

    broken user score route

--- controllers/htmlroutes.js/index.js
+++ controllers/htmlroutes.js/index.js
@@ -1,51 +1,81 @@
-const { Score, User } = require('../../models'); 
+const { Score, User } = require('../../models');
 const router = require('express').Router();
 
-router.get('/', (req, res)=>{
-    res.render('home');
+router.get('/', (req, res) => {
+  res.render('home');
 });
 
-router.get('/login', (req, res)=>{
-    res.render('login');
+router.get('/login', (req, res) => {
+  res.render('login');
 });
 
-router.get('/signup', (req, res)=>{
-    res.render('signup');
+router.get('/signup', (req, res) => {
+  res.render('signup');
 });
 
-router.get('/trivia', (req, res)=>{
-    res.render('trivia');
+router.get('/trivia', (req, res) => {
+  res.render('trivia');
 });
 
 router.get('/highscores', async (req, res) => {
-    try {
-      // Get all scores and JOIN with user data
-      const scoreData = await Score.findAll({
-        order: [
-            ['game_score', 'DESC']
-        ],
-        include: [
-          {
-            model: User,
-            attributes: ['username'],
-          },
-        ],
-      });
-  
-      // Serialize data so the template can read it
-      const scores = scoreData.map((score) => score.get({ plain: true }));
-  
-      // Pass serialized data and session flag into template
-      res.render('highscores', { 
-        scores, 
-        logged_in: req.session.logged_in 
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+  try {
+    // Get all scores and JOIN with user data
+    const scoreData = await Score.findAll({
+      order: [
+        ['game_score', 'DESC']
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
 
-// router.get('/scores', (req,res) => {
-    
+    // Serialize data so the template can read it
+    const scores = scoreData.map((score) => score.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('highscores', {
+      scores,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//how to link this to the user ID??
+router.get('/users/:id', async (req, res) => {
+  try {
+    // Get all user scores and JOIN with user data
+    const scoreData = await User.findByPk(req.params.id, {
+      
+      // order: [
+      //     ['DESC']
+      // ],
+      include: [
+        {
+          model: Score,
+          attributes: ['game_score', 'date_created', 'time_created'],
+          // order: [
+          //   ['time_created', 'DESC']
+          // ],
+        },
+      ],
+    });
+    console.log(scoreData);
+    // Serialize data so the template can read it
+    const scores = scoreData.map((score) => score.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('userscores', {
+      ...scores,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // });
-module.exports= router;
+module.exports = router;
