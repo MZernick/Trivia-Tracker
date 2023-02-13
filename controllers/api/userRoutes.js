fd3cd61ce3804ea7c386ceac52b68beee408d797
unknown <mescamilla1114@gmail.com>
2023-02-13 15:00:16 -0600

    says my uroutes are diffent even though they are exactly the same. should be fun

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -23,9 +23,9 @@ router.post('/signup', async (req, res) => {
       // req.session.password = password,
       req.session.logged_in = true;
 
-  //     res.status(200).json(userData);
-  //     console.log(userData);
-  //   });
+      res.status(200).json(userData);
+      console.log(userData);
+    });
   } catch (err) {
   
     // res.status(400).json(err);
