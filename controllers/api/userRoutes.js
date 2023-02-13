5d55aadad6412b470e51c448a18c394b3b2f6337
unknown <mescamilla1114@gmail.com>
2023-02-13 08:36:30 -0600

    changes is userroutes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 const { User } = require('../../models');
 //check to see if user is already logged in
 
-// GET all user - WORKS
+// GET all user
 router.get('/', async (req, res) => {
+  res.render('home');
   try {
-    // res.render('home');
-    const userData = await User.findAll();    
+    res.render('home');
+    const userData = await User.findAll();
+    
     res.status(200).json(userData);
   } catch (err) {
     res.status(500).json(err);
@@ -17,10 +19,9 @@ router.post('/signup', async (req, res) => {
   try {
     const userData = await User.create(req.body);
 
-    req.body.save(() => {
-      req.body.username = username,
-      req.body.password = password,
-      // req.session.logged_in = true;
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
 
       res.status(200).json(userData);
       console.log(userData);
@@ -30,8 +31,18 @@ router.post('/signup', async (req, res) => {
   }
 });
 
+/*router.get('/', async (req, res) => {
+  try {
+    const userData = await User.findAll();
+    res.render('home');
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});*/
+
 //on the login page and rejects not found email
-router.post('/login', async (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
 
@@ -66,7 +77,7 @@ router.post('/login', async (req, res) => {
 });
 
 //what's page/ current score?
-router.get('/highscores', async (req, res) => {
+router.post('/highscores', async (req, res) => {
   //something like 
   //if 
   //const newUserHighscore=  req.session.user_high_Score
